e97eab7b966520b7ddc54ba82dc56de0acb1397f
maggietraynor <mtraynor@itba.edu.ar>
2023-11-25 19:56:10 -0300

    ya casi!!!

---
+++ consulta.js
@@ -0,0 +1,6 @@
+let dat = document.getElementById("enviarDatos");
+dat.addEventListener("click", consultaEnviada);
+
+function consultaEnviada(){
+  alert("¡Gracias por enviar tu consulta! En la brevedad estaremos respondiendote vía mail.");
+}
--- script.js
+++ script.js
@@ -1,95 +1,7 @@
-let dat = document.getElementById("enviarDatos");
-dat.addEventListener("click", consultaEnviada);
+document.addEventListener("DOMContentLoaded", function () {
 
-function consultaEnviada(){
-  alert("¡Gracias por enviar tu consulta! En la brevedad estaremos respondiendote vía mail.");
-}
-
-
-// Variable para almacenar los productos en el carrito
-let productosEnCarrito = [];
-
-// Función para agregar un producto al carrito
-function agregarAlCarrito(idProducto) {
-  // Buscar el producto en el carrito por su ID
-  let producto = productosEnCarrito.find(function(item) {
-    return item.id === idProducto;
-  });
-
-  // Si el producto no existe en el carrito, agregarlo con cantidad 1
-  if (!producto) {
-    productosEnCarrito.push({ id: idProducto, cantidad: 1 });
-  } else {
-    // Si el producto ya existe, incrementar la cantidad
-    producto.cantidad++;
-  }
-
-  // Actualizar el carrito en la página
-  actualizarCarrito();
-}
-
-function quitarDelCarrito(idProducto) {
-    // Buscar el producto en el carrito por su ID
-    let indiceProducto = productosEnCarrito.findIndex(function(item) {
-      return item.id === idProducto;
-    });
-  
-    // Si el producto existe en el carrito, reducir la cantidad
-    if (indiceProducto !== -1) {
-      productosEnCarrito[indiceProducto].cantidad--;
-  
-      // Si el producto esta solo una vez, eliminar el producto del carrito
-      if (productosEnCarrito[indiceProducto].cantidad === 0) {
-        productosEnCarrito.splice(indiceProducto, 1);
-      }
-    }
-  
-    // Actualizar el carrito en la página
-    actualizarCarrito();
-  }
-  
-
-// Función para actualizar el carrito en la página
-function actualizarCarrito() {
-    let elementoProductosEnCarrito = document.getElementById('productos-en-carrito');
-    let precioTotal = 0;
-  
-    // Limpiar el contenido actual del carrito
-    elementoProductosEnCarrito.innerHTML = '';
-  
-    // Recorrer los productos en el carrito y agregarlos al HTML
-    productosEnCarrito.forEach(function(item) {
-        let producto = obtenerProductoPorId(item.id);
-        let precioProducto = calcularPrecioProducto(producto, item.cantidad);
-        precioTotal += precioProducto;
-    
-        let li = document.createElement('li');
-        li.textContent = producto.nombre + ' x ' + item.cantidad + ' - $' + precioProducto.toFixed(2);
-        li.classList.add("elemento-Carrito")
-    
-        // Agrega un botón de eliminación al elemento li
-        let botonEliminar = document.createElement('button');
-        botonEliminar.textContent = 'Eliminar';
-        botonEliminar.setAttribute('data-id-producto', item.id);
-        botonEliminar.classList.add("elemento-Eliminar")
-        botonEliminar.addEventListener('click', function() {
-        let idProducto = parseInt(this.getAttribute('data-id-producto'));
-        quitarDelCarrito(idProducto);
-        });
-  
-        li.appendChild(botonEliminar);
-        elementoProductosEnCarrito.appendChild(li);
-    });
-  
-  
-    // Mostrar el precio total acumulado en el carrito
-    let elementoTotal = document.createElement('ul');
-    elementoTotal.textContent = 'Total: $' + precioTotal.toFixed(2);
-    elementoProductosEnCarrito.appendChild(elementoTotal);
-  }
-  
-// Función auxiliar para obtener un producto por su ID
-function obtenerProductoPorId(idProducto) {
+  // Función auxiliar para obtener un producto por su ID
+  function obtenerProductoPorId(idProducto) {
     let productos = [
       { id: 1, nombre:  'D25 - Pinot Noir', precio: 28050},
       { id: 2, nombre: 'D25 - Cabernet Franc', precio: 31160},
@@ -112,36 +24,147 @@ function obtenerProductoPorId(idProducto) {
       { id: 19, nombre: 'PM - Blanc de Noirs', precio: 36300},
       { id: 20, nombre: 'PM - Chardonnay', precio: 36300},
     ];
-  
+
     return productos.find(function(producto) {
       return producto.id === idProducto;
     });
   }
-  
 
-// Función auxiliar para calcular el precio de un producto en función de su cantidad
-function calcularPrecioProducto(producto, cantidad) {
-  return producto.precio * cantidad;
-}
+  // Variable para almacenar los productos en el carrito
+  let productosEnCarrito = [];
 
-function vaciarCarrito() {
-    // Vaciar el arreglo de productos en el carrito
-    productosEnCarrito = [];
-  
+  // Función para agregar un producto al carrito
+  function agregarAlCarrito(idProducto) {
+    // Buscar el producto en el carrito por su ID
+    let producto = productosEnCarrito.find(function(item) {
+      return item.id === idProducto;
+    });
+
+    // Si el producto no existe en el carrito, agregarlo con cantidad 1
+    if (!producto) {
+      productosEnCarrito.push({ id: idProducto, cantidad: 1 });
+    } else {
+      // Si el producto ya existe, incrementar la cantidad
+      producto.cantidad++;
+    }
+
     // Actualizar el carrito en la página
     actualizarCarrito();
-}
-
-function finalizarCompra() {
-    // Verificar si el carrito está vacío
-    if (productosEnCarrito.length === 0) {
-      alert("El carrito está vacío");
-      return; // Salir de la función sin continuar con la finalización de la compra
-    }
-      
-    // Mostrar un mensaje al usuario
-    alert("Su compra se ha realizado con éxito");
-  
-    // Vaciar el carrito
-    vaciarCarrito();
   }
+
+  function quitarDelCarrito(idProducto) {
+      // Buscar el producto en el carrito por su ID
+      let indiceProducto = productosEnCarrito.findIndex(function(item) {
+        return item.id === idProducto;
+      });
+    
+      // Si el producto existe en el carrito, reducir la cantidad
+      if (indiceProducto !== -1) {
+        productosEnCarrito[indiceProducto].cantidad--;
+    
+        // Si el producto esta solo una vez, eliminar el producto del carrito
+        if (productosEnCarrito[indiceProducto].cantidad === 0) {
+          productosEnCarrito.splice(indiceProducto, 1);
+        }
+      }
+    
+      // Actualizar el carrito en la página
+      actualizarCarrito();
+    }
+    
+    let botonesAgregarCarrito = document.querySelectorAll('.btn-agregar-carrito');
+
+        // Asigna el manejador de evento a cada botón
+        botonesAgregarCarrito.forEach(function (boton) {
+            boton.addEventListener('click', function () {
+                // Obten el ID del producto desde algún atributo del botón, por ejemplo, data-id
+                let idProducto = parseInt(this.getAttribute('data-id-producto'));
+                
+                // Llama a la función agregarAlCarrito
+                agregarAlCarrito(idProducto);
+            });
+        });
+
+  // Función para actualizar el carrito en la página
+  function actualizarCarrito() {
+      let elementoProductosEnCarrito = document.getElementById('productos-en-carrito');
+      let precioTotal = 0;
+    
+      // Limpiar el contenido actual del carrito
+      elementoProductosEnCarrito.innerHTML = '';
+    
+      // Recorrer los productos en el carrito y agregarlos al HTML
+      productosEnCarrito.forEach(function(item) {
+          let producto = obtenerProductoPorId(item.id);
+          let precioProducto = calcularPrecioProducto(producto, item.cantidad);
+          precioTotal += precioProducto;
+      
+          let li = document.createElement('li');
+          li.textContent = producto.nombre + ' x ' + item.cantidad + ' - $' + precioProducto.toFixed(2);
+          li.classList.add("elemento-Carrito")
+      
+          // Agrega un un span con las clases de Bootstrap para un botón de eliminar
+          let spanEliminar = document.createElement('span');
+          spanEliminar.textContent = 'Eliminar';
+          spanEliminar.setAttribute('data-id-producto', item.id);
+          spanEliminar.classList.add("elemento-Eliminar", "btn", "btn-primary");
+          spanEliminar.addEventListener('click', function() {
+              let idProducto = parseInt(this.getAttribute('data-id-producto'));
+              quitarDelCarrito(idProducto);
+          });
+  
+          li.appendChild(spanEliminar);
+          elementoProductosEnCarrito.appendChild(li);
+      });
+    
+    
+      // Mostrar el precio total acumulado en el carrito
+      let elementoTotal = document.createElement('ul');
+      elementoTotal.textContent = 'Total: $' + precioTotal.toFixed(2);
+      elementoProductosEnCarrito.appendChild(elementoTotal);
+    }
+    
+
+
+  // Función auxiliar para calcular el precio de un producto en función de su cantidad
+  function calcularPrecioProducto(producto, cantidad) {
+    return producto.precio * cantidad;
+  }
+
+  function vaciarCarrito() {
+      // Vaciar el arreglo de productos en el carrito
+      productosEnCarrito = [];
+    
+      // Actualizar el carrito en la página
+      actualizarCarrito();
+  }
+
+  function finalizarCompra() {
+      // Verificar si el carrito está vacío
+      if (productosEnCarrito.length === 0) {
+        alert("El carrito está vacío");
+        return; // Salir de la función sin continuar con la finalización de la compra
+      }
+        
+      // Mostrar un mensaje al usuario
+      alert("Su compra se ha realizado con éxito");
+    
+      // Vaciar el carrito
+      vaciarCarrito();
+    }
+
+    let botonesFinalizarCompra = document.querySelectorAll('.btn-finalizar-compra');
+        botonesFinalizarCompra.forEach(function (boton) {
+            boton.addEventListener('click', function () {
+                finalizarCompra()
+            });
+        });
+    let botonesVaciarCompra = document.querySelectorAll('.btn-vaciar-carrito');
+    botonesVaciarCompra.forEach(function (boton) {
+        boton.addEventListener('click', function () {
+            vaciarCarrito()
+        });
+    });
+});
+
+
